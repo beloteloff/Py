54c11fb1a108c03c9964e192f72b148170c4c6d8
VV <20948894+beloteloff@users.noreply.github.com>
2020-07-20 20:31:48 +0200

    Update sql_to_file.py

--- sql_to_file.py
+++ sql_to_file.py
@@ -3,7 +3,7 @@ os.environ["NLS_LANG"] = "Russian.AL32UTF8" #"American_America.AL32UTF8" ".UTF8"
 
 dsnStr = cx_Oracle.makedsn("b42.brand.local", "1521", "birg")
 
-connection = cx_Oracle.connect(user="user", password="Pass", dsn=dsnStr,encoding='utf-8')
+connection = cx_Oracle.connect(user="user", password="Pass", dsn=dsnStr, encoding='utf-8')
 curs = connection.cursor()
 
 sql = ''' SELECT * FROM db.table WHERE rownum < 3 '''
@@ -13,7 +13,7 @@ with open('output_file.csv', 'w') as f:
     f.write('|'.join(str(col[0]) for col in curs.description) + "\n")
     for row in curs:
         #print(''.join(str(row[9]) ))
-        print(*row ,sep='|', file=f) # get all elements from tuple
+        print(*row, sep='|', file=f) # get all elements from tuple
         
 connection.close()
 f.close()
